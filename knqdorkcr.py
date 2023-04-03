3f1201afec7d1d15ec279a7fc896bd3bf22e8605
knqsec <124434945+knqsec@users.noreply.github.com>
2023-04-03 12:39:31 +0300

    Add files via upload

---
+++ knqdorkcr.py
@@ -0,0 +1,52 @@
+from colorama import *
+import os
+import webbrowser
+import requests
+from bs4 import BeautifulSoup
+
+
+
+
+
+benner = """
+                       ...:::::::::::::::::::::::::.                  
+               .^^:..!?JY5PGBBB5YYYYYYYYY5PPG#&@@&&&G?:               
+           :!YGP5PP55YYJ?JY5YJ?J???JYYJJJYY55YY5B&@@@@&5:             
+         7G&@@@GGGGP55YJJY5PPGB&&&BYYYYYYJ?!7JY5YJYP#@@@&7            
+        P@@@@@G?YGBB###GYG&@@@@@@B^Y&@@@@@&&BPY?JPPPYG@@@@J           
+       !@@@@@&P&@@@@@@@@JJ@@@@@@@PP@&#BGP555GB&&GG##@@@@@@@7          
+      J&@@@@&@&PJ?7!!75&BG@@@@@@@@P?J5GG7     !?5#@&&@@@@&##5~        
+    7GBPPGGGP&G!~77~....!5YY@@@@@!.GGJ7JJ!!!77Y?:?&5JB#BBY?J5Y!:      
+  ^BPYYPGBB#&@@@@@&&&&&B^^5B@@@@@B5Y?JG@@@5!?G##&&#Y!~!?????JGP~^:    
+  ?B?P5#7!?J?~!5BYP@@@@&!7@@@@@@@@@@@@@@@@@&GYY5J?775&&?G@@&P~P&^G^   
+  ~JP5J@@@@5BBJ7JP&@&G?!JB@@@@@@@@5JY#GB&&&&@@@@@@@#PJ!:^YG&@P^@!#J   
+   ^YBYB@@7^B@@@@&G5~.:&@@@@@@BPP5P#^^#GBBB#&#BPY?7?YB@# .JJ#J?&:#~   
+    7BGP&7.!.7P&#G#@#@GJP#B&@@BP5YPG!5&B5J?!!!7! J@@&BY.:#@@&YG??!    
+     ?@@P  ^B~:!!?Y5GB#BY?JB###GPYJ7!!:^YPB#@@G! :!!~. ^#@@@P557:     
+      P@5  ~&:P@5 !5YJ?^:?JJY7:J5PG#&@J^&BGY7^:. YBB7 !&@@@@@P^       
+      ?@B   : ~P! 5B###7:B##&P.B#BGP5Y^ . .^?G&B.5?.:5@@@@@@!         
+      7@@~                       .:~?Y5.:G&@@@@&^ ^5&@@@@@@7          
+      5@@5 J^ ~ .:  ^^.  :7777! Y#&@@@@P.P@@#57~JB@@@@@@@5^           
+     ~@@@@J~^^#P^##!Y@&B~!@@@@@^P@@@@@@@J !!7JG&&@@@B@@P~             
+     J@@@@@BJ!!?.~5J JGBY YGGGP:^?77!!!!77JY5YYY5GBG5J^               
+     Y@@B&@@PP&##BGPPJJJJJJJYYJYYJJJJJ?7777?JY555J7^                  
+     Y@@YG@@#??JYY55Y!777777!!!YJJ?7777JY5PY?!~:.                     
+     7@@@#GY5PP5YYJJJYYYYYYYYYYJJYYPGB#B5?^.                          
+      7B@@@&BP555PGBB####BBBBBBB###GY7^.                              
+        ~JPB#&@@@@@&&#BGPY7!~^^:..                                    
+            ..::::::..                                                                                                                    
+"""
+
+os.system('title Dork Creator by Knq' if os.name == 'posix' else 'title Dork Creator By Knq')
+os.system('clear' if os.name == 'posix' else 'cls')
+print(Fore.LIGHTRED_EX + benner + Style.RESET_ALL)
+print(Fore.LIGHTCYAN_EX + 'Dork Creator By KNQSEC' + Style.RESET_ALL)
+print(Fore.LIGHTMAGENTA_EX + 'Site turu Sudur => com.tr - gov.tr - com - edu.tr gibi gibi' + Style.RESET_ALL)
+sitesecimi = input(Fore.RED + 'Site Tur Secimi: ' + Style.RESET_ALL)
+print(Fore.LIGHTMAGENTA_EX + 'Dork methodu Sudur => id - search - ara gibi gibi' + Style.RESET_ALL)
+dorkm = input(Fore.RED + 'Dork Method Secimi: ' + Style.RESET_ALL)
+print(Fore.RED + 'Dorkunuz Olusturuldu => ' + Style.RESET_ALL + Fore.LIGHTRED_EX + 'inurl:”php?' + dorkm + '=” “' + sitesecimi + '”' + Style.RESET_ALL)
+geridonmece = input(Fore.LIGHTCYAN_EX + 'Cikmak icin Entere Bas' + Style.RESET_ALL)
+           
+           
+    
